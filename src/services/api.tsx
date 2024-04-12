798c83cd4990889bc4f99e452f7847e9ed753216
evertonjose421@gmail.com <evertonjose421@gmail.com>
2024-04-12 18:02:13 -0300

    12/04

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,8 +2,8 @@
 import axios from "axios";
 
     export const api = axios.create({
-        baseURL:"http://100.100.226.100:3000",
+        baseURL:"http://000000000000000:3000",
         headers:{
-            "authorization": "token h43895jt9858094bun6098grubn48u59",
+            "authorization": "token 000000000000000000000",
         }
     })
